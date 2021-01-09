e5a88794faf611b4a9fcf0a3f9796aa7bcf4397d
Shrew <Shrew@LAPTOP-RNIG8QT8>
2021-01-09 16:54:46 +0530

    #master - with customized url path

--- PhotoAppApiUsers/src/main/java/com/apps/photoapp/users/security/WebSecurity.java
+++ PhotoAppApiUsers/src/main/java/com/apps/photoapp/users/security/WebSecurity.java
@@ -2,24 +2,46 @@ package com.apps.photoapp.users.security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.apps.photoapp.users.service.UsersService;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
 	private Environment environment;
+	private UsersService usersService;
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
 
-	public WebSecurity(Environment environment) {
+	public WebSecurity(Environment environment, UsersService usersService,
+			BCryptPasswordEncoder bCryptPasswordEncoder) {
 		this.environment = environment;
+		this.usersService = usersService;
+		this.bCryptPasswordEncoder = bCryptPasswordEncoder;
 	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable();
 		// http.authorizeRequests().antMatchers("/users/**").permitAll();
-		http.authorizeRequests().antMatchers("/users/**").hasIpAddress(environment.getProperty("gateway.ip"));
+		http.authorizeRequests().antMatchers("/users/**").hasIpAddress(environment.getProperty("gateway.ip")).and()
+				.addFilter(getAuthenticationFilter());
+	}
+
+	private AuthenticationFilter getAuthenticationFilter() throws Exception {
+		AuthenticationFilter authenticationFilter = new AuthenticationFilter(usersService, environment, authenticationManager());
+		// authenticationFilter.setAuthenticationManager(authenticationManager());
+		authenticationFilter.setFilterProcessesUrl(environment.getProperty("login.url.path"));
+		return authenticationFilter;
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(usersService).passwordEncoder(bCryptPasswordEncoder);
 	}
 
 }
--- PhotoAppApiUsers/src/main/java/com/apps/photoapp/users/service/UsersService.java
+++ PhotoAppApiUsers/src/main/java/com/apps/photoapp/users/service/UsersService.java
@@ -1,8 +1,12 @@
 package com.apps.photoapp.users.service;
 
+import org.springframework.security.core.userdetails.UserDetailsService;
+
 import com.apps.photoapp.users.shared.UserDto;
 
-public interface UsersService {
+public interface UsersService extends UserDetailsService{
 
 	UserDto createUser(UserDto userDetails);
+	
+	UserDto getUserDetailsByEmail(String email);
 }
