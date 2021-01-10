89a3d9df13d2310896475ce94c3f67ae2b95c26c
Shrew <Shrew@LAPTOP-RNIG8QT8>
2021-01-10 19:05:42 +0530

    #master - Adding Config

--- PhotoAppApiConfigServer/src/main/java/com/apps/photoapp/api/PhotoAppApiConfigServerApplication.java
+++ PhotoAppApiConfigServer/src/main/java/com/apps/photoapp/api/PhotoAppApiConfigServerApplication.java
@@ -2,10 +2,8 @@ package com.apps.photoapp.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-@EnableConfigServer
 public class PhotoAppApiConfigServerApplication {
 
 	public static void main(String[] args) {
